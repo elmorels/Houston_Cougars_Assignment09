e32cac8ce49241cf880f33d2979e8702b981e493
Luke Elmore <elmorels@mail.uc.edu>
2025-04-01 07:57:29 -0400

    Add project files.

---
+++ Houston_Cougars_Assignment09/DatabaseManagementPackage/DataBaseManagement.py
@@ -0,0 +1,21 @@
+#DatabaseManagement.py
+#Luke Elmore
+
+import pyodbc
+
+class DatabaseManagement:
+    def connectToDatabase(self):
+        '''
+        Establishes a connection to our secret service database
+        @Return Connection object: The connection object
+        '''
+    
+        conn = pyodbc.connect('Driver={SQL Server};'
+                'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                'Database=GroceryStoreSimulator;'
+                'uid=IS4010Login;'
+                'pwd=P@ssword2;')
+
+        return conn
+
+
---
+++ Houston_Cougars_Assignment09/MainPackage/Main.py
@@ -0,0 +1,9 @@
+#Main.py
+# Luke Elmore
+
+from DatabaseManagementPackage.DataBaseManagement import *
+
+dbm = DatabaseManagement()
+conn = dbm.connectToDatabase()
+
+
